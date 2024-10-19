88459cc4e95b16f0542b4d6ad46111bc6dd9cfe2
Harry Wixley <hwixley1@gmail.com>
2024-10-19 19:25:57 +0100

    GPT-commit: Fix calculation of language size in generate_images script.
    Refactor how language sizes are calculated in generate_images.

--- generate_images.py
+++ generate_images.py
@@ -61,8 +61,10 @@ async def generate_languages(s: Stats) -> None:
 
     default_lang = {"size": 0}
     lang_aliases = { "Python": "Jupyter Notebook", "PHP": "Blade"}
+    print(langs)
     for key, value in lang_aliases.items():
-        langs[key]["size"] = langs.get(key, default_lang)["size"] + langs.get(value, default_lang)["size"]/15
+        lang_size = langs.get(key, default_lang)["size"] if key in langs.keys() else 0
+        langs[key]["size"] = lang_size + langs.get(value, default_lang)["size"]/15
         langs.pop(value, None)
     langs_total = sum([v.get("size", 0) for v in langs.values()])
     for key, value in langs.items():
